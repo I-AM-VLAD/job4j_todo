b15f5bb6d25db753766e7cb8f896c0c0490fefb4
I-AM-VLAD <vlad1slaw19982@gmail.com>
2025-02-01 00:53:45 +0200

    5. Аутентификация в проекте TODO [#504877]

---
+++ src/main/java/ru/job4j/todo/model/User.java
@@ -0,0 +1,22 @@
+package ru.job4j.todo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "todo_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String login;
+    private String password;
+}
---
+++ src/main/java/ru/job4j/todo/repository/HibernateUserRepository.java
@@ -0,0 +1,69 @@
+package ru.job4j.todo.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+import ru.job4j.todo.model.Task;
+import ru.job4j.todo.model.User;
+
+import java.util.Optional;
+
+@Repository
+@AllArgsConstructor
+public class HibernateUserRepository implements UserRepository {
+
+    private final SessionFactory sf;
+
+    @Override
+    public Optional<User> save(User user) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sf.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            return Optional.of(user);
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByLoginAndPassword(String login, String password) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = sf.openSession();
+            transaction = session.beginTransaction();
+
+            Query<User> query = session.createQuery(
+                    "from User as u where u.login = :login and u.password = :password", User.class);
+            query.setParameter("login", login);
+            query.setParameter("password", password);
+            Optional<User> result = query.uniqueResultOptional();
+
+            transaction.commit();
+            return result;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
